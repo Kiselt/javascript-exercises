f888a7eb4062e8d22a0810e3d2ba618838132603
Kiselt <157617885+Kiselt@users.noreply.github.com>
2024-02-01 18:26:59 +0100

    solved

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  if (year % 4 === 0 && year % 100 === 0) {
+    if (year % 4 === 0 && year % 100 === 0 && year % 400 === 300) {
+      return false
+    }
+      return true
+    } else if (year % 400 <= 396) {
+        return true 
+    } else {
+      return false
+    }
 };
-
 // Do not edit below this line
 module.exports = leapYears;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let calculation = ((fahrenheit - 32) * 5) / 9;
+  if (Number.isInteger(calculation) === false) {
+    return Number(calculation.toFixed(1));
+  } else {
+    return calculation;
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const calculation = ((celsius * 9) / 5) + 32;
+  if (Number.isInteger(calculation) === false) {
+    return Number(calculation.toFixed(1));
+  } else {
+    return calculation;
+  }
 };
 
 // Do not edit below this line
